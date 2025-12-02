9bd5e137f8e092fc1d1a39aba6c3edbc9436ead0
Sonali kumbhar <sonalikumbhar09@gmail.com>
2025-12-03 01:20:56 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostCardComponent } from './shared/component/post-card/post-card.component';
 import {HttpClientModule} from "@angular/common/http";
-import { GetConfirmComponent } from './shared/component/get-confirm/get-confirm.component'
+import { GetConfirmComponent } from './shared/component/get-confirm/get-confirm.component';
+import { TestComponent } from './shared/component/test/test.component';
+import { AuthComponent } from './shared/component/auth/auth.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCardComponent,
-    GetConfirmComponent
+    GetConfirmComponent,
+    TestComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
